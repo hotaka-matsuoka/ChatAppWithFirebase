44abc9cde1ab1fb35920012f5136138b6cfcf42f
松岡穂高 <hotaka_matsuoka@MacBookAir.local>
2021-02-01 15:05:52 +0900

    initial commit

---
+++ ChatAppWithFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by 松岡穂高 on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
